bfee91a3c0f1f5b81d0d60637eaeea2377b6e2d5
mertdurukan <35916889+mertdurukan@users.noreply.github.com>
2022-07-15 13:06:51 +0300

    Basic Level 1
    Explained how it works.

---
+++ DenemeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PO_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DenemeController : Controller
+    {
+
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            return Ok("Deneme çalıştı");
+        }
+
+        [HttpGet("ogrenci")]
+        public IActionResult EkranaOgrencileriYaz()
+        {
+            return BadRequest("Başarısız");
+        }
+
+         // belirli özelliklere sahip birden çok olayım varsa 
+         // ben bir interface tanımlayıp
+         // o olayları interfaceden miras aldırırım
+         // kalıtım alan class(sınıf) interfacedeki fonksiyon ya da değişkenleri
+         // kendi parentezlerinde gerçeklemek zorunda
+    }
+}
---
+++ GenelController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PO_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenelController : Controller
+    {
+
+        // https://localhost:44301/Genel
+
+        public IActionResult Index()
+        {
+            return Ok("x");
+        }
+    }
+}
